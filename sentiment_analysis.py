fb0a3d145386846e475c5c1d09df959c0367e102
Sevval <41107229+sevvalkahraman@users.noreply.github.com>
2019-05-04 17:50:07 +0300

    Update sentiment_analysis.py
    Delivery 4, final retouch.

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -24,24 +24,6 @@ import matplotlib.pyplot as plt
 
 
 
-from wordcloud import WordCloud,STOPWORDS
-
-def wordcloud_draw(data, color = 'black'):
-    words = ' '.join(data)
-    cleaned_word = " ".join([word for word in words.split()
-                            if not word.startswith(',')
-                            ])
-    wordcloud = WordCloud(stopwords=STOPWORDS,
-                    background_color=color,
-                    width=2500,
-                    height=2000
-                    ).generate(cleaned_word)
-    plt.figure(1,figsize=(13, 13))
-    plt.imshow(wordcloud)
-    plt.axis('off')
-    plt.show()
-
-
 
 def main():
 
@@ -51,204 +33,120 @@ def main():
     print(dataset.info())
     print("----------------------------")
 
-
     #print(dataset.head() )
     # print(dataset['Class'])
+
+    #Lowercase
+    dataset= dataset.applymap(lambda s:s.lower() if type(s) == str else s)
 
     #Recovery
     dataset['Sentence'] = dataset['Sentence'].str.replace(r'\'s', '')
     dataset['Sentence'] = dataset['Sentence'].str.replace(r'.', '')
     dataset['Sentence'] = dataset['Sentence'].str.replace(r',', '')
-    # dataset['Sentence'] = dataset['Sentence'].str.replace(r'does n\'t', 'does not')
-    # dataset['Sentence'] = dataset['Sentence'].str.replace(r'is n\'t', 'is not')
-    # dataset['Sentence'] = dataset['Sentence'].str.replace(r'were n\'t', 'were not')
-    # dataset['Sentence'] = dataset['Sentence'].str.replace(r'are n\'t', 'are not')
-    # dataset['Sentence'] = dataset['Sentence'].str.replace(r'had n\'t', 'had not')
-    # dataset['Sentence'] = dataset['Sentence'].str.replace(r'have n\'t', 'have not')
-    # dataset['Sentence'] = dataset['Sentence'].str.replace(r'would n\'t', 'would not')
-    # dataset['Sentence'] = dataset['Sentence'].str.replace(r'ca n\'t', 'can not')
-    # dataset['Sentence'] = dataset['Sentence'].str.replace(r'could n\'t', 'could not')
-    # dataset['Sentence'] = dataset['Sentence'].str.replace(r'must n\'t', 'must not')
-    # dataset['Sentence'] = dataset['Sentence'].str.replace(r'should n\'t', 'should not')
-    # dataset['Sentence'] = dataset['Sentence'].str.replace(r'wo n\'t', 'will not')
-    # dataset['Sentence'] = dataset['Sentence'].str.replace(r'n\'t', 'not')
-
-
-
-    #print(dataset['Sentence'])
-
-    #Split words and turn to lower case, then count words in all sentences.
-    words = Counter(" ".join(dataset['Sentence'].str.lower().values.tolist()).split(" ")).items()
-
-    #dict_items to the list
-    list_words = list(words)
-
-    #Sort the list according to the second elements.
-    sorted_list_words = sorted(list_words, key=lambda l:l[1], reverse=True)
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'i\'ve', 'i have')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'i\'m', 'i am')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'didn\'t', 'did not')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'don\'t', 'do not')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'does n\'t', 'does not')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'is n\'t', 'is not')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'were n\'t', 'were not')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'are n\'t', 'are not')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'had n\'t', 'had not')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'have n\'t', 'have not')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'would n\'t', 'would not')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'ca n\'t', 'can not')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'could n\'t', 'could not')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'must n\'t', 'must not')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'should n\'t', 'should not')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'wo n\'t', 'will not')
+    dataset['Sentence'] = dataset['Sentence'].str.replace(r'n\'t', 'not')
 
    
     #Defining stop words
     stop = set(stopwords.words('english'))
     #Add some neccessary words.
     stop.add("") #add empty word into stop set.
-    stop.add("-") 
-    stop.add("i'm") 
-    stop.add("i've") 
-    #print (stop)
-
-
-    # #Removing stop words
-    # removed_stop_words = []
-    # for i in range(len(sorted_list_words)):
-    #     if sorted_list_words[i][0] not in stop :
-    #         removed_stop_words.append([]) #2nd dimension
-    #         length = len(removed_stop_words) #current length of array
-    #         removed_stop_words[length-1].append(sorted_list_words[i][0])
-    #         removed_stop_words[length-1].append(sorted_list_words[i][1])
-
-    # text_file = open("latex_table_format.txt", "w")
-    # #print most used 50 words
-    # for i in range(50):
-    #     print("{}. word is '{}' : {}" .format(i, removed_stop_words[i][0], removed_stop_words[i][1] ))
-    #     text_file.write("\hline\n%s & %d r'\\'\n" %(removed_stop_words[i][0],removed_stop_words[i][1]))
-            
-    #print("Before deleting stop words size is : {}, After deleting stop words size is: {} " .format(len(sorted_list_words), len(removed_stop_words)))
-
-
-    # print(dataset['Sentence'])
-    # data = data.rename(columns={'Sentence ': 'Sentence'})
-    # data = data.rename(columns={'Class ': 'Class'})
-
-    #Class labels
-    y_train = dataset['Class']
-
-    from sklearn.feature_extraction.text import CountVectorizer
-
-    cv = CountVectorizer(binary=True)
-    cv.fit(dataset.Sentence)
-    X = cv.transform(dataset.Sentence)
-    #X_test = cv.transform(dataset.Sentence)
-
-    #neg = 0
-    #pos = 1
-    #dataset_pos = dataset[dataset.Class.isin([pos])] #positive sentences-class
-    #dataset_neg = dataset[dataset.Class.isin([neg])] #negative sentences-class
-    #print(dataset_pos)
-    #print(dataset_neg)
-
-    # print("Positive words")
-    # wordcloud_draw(dataset_pos,'white')
-    # print("Negative words")
-    # wordcloud_draw(dataset_neg)
-
-
-
-    #Count words as positive or negative.(TERM FREQUENCY)
-    neg_doc_mat = dataset.Sentence[dataset['Class'] == 0]
-    neg_document_matrix = cv.transform(dataset.Sentence[dataset['Class'] == 0])
-
-    pos_doc_mat = dataset.Sentence[dataset['Class'] == 1]
-    pos_document_matrix = cv.transform(pos_doc_mat)
-    
-    neg_batches = np.linspace(0,156061,100).astype(int)
-    i=0
-    neg_tf = []
-    while i < len(neg_batches)-1:
-        batch_result = np.sum(neg_document_matrix[neg_batches[i]:neg_batches[i+1]].toarray(),axis=0)
-        neg_tf.append(batch_result)
-        if (i % 10 == 0) | (i == len(neg_batches)-2):
-            print(neg_batches[i+1],"entries' term frequency calculated")
-        i += 1
-
-    pos_batches = np.linspace(0,156061,100).astype(int)
-    i=0
-    pos_tf = []
-    while i < len(pos_batches)-1:
-        batch_result = np.sum(pos_document_matrix[pos_batches[i]:pos_batches[i+1]].toarray(),axis=0)
-        pos_tf.append(batch_result)
-        if (i % 10 == 0) | (i == len(pos_batches)-2):
-            print(pos_batches[i+1],"entries' term frequency calculated")
-        i += 1
-
-
-
-    #Create dataframe
-    neg = np.sum(neg_tf,axis=0)
-    pos = np.sum(pos_tf,axis=0)
-    term_freq_df = pd.DataFrame([neg,pos],columns=cv.get_feature_names()).transpose()
-    term_freq_df.head()
-
-
-    term_freq_df.columns = ['negative','positive']
-    term_freq_df['total'] = term_freq_df['negative'] + term_freq_df['positive']
-    print (term_freq_df.sort_values(by='total', ascending=False).iloc[:10])
+    stop.add("-")
+    stop.add("would")
+    stop.add("could")    
 
 
     #Create CountVectorizer for deleting stopwords
     from sklearn.feature_extraction.text import CountVectorizer
-    cvec = CountVectorizer(stop_words='english',max_features=10000)
+    cvec = CountVectorizer(stop_words=stop, max_features=10000)
     cvec.fit(dataset.Sentence)
 
 
     #Count words as positive or negative after deleting stop words.(TERM FREQUENCY) 
+    #Get negative sentences -> 0
     neg_document_matrix_nostop = cvec.transform(dataset.Sentence[dataset['Class'] == 0])
     
+    #Get negative words into array.
     neg_batches = np.linspace(0,156061,100).astype(int)
     i=0
     neg_tf = []
     while i < len(neg_batches)-1:
         batch_result = np.sum(neg_document_matrix_nostop[neg_batches[i]:neg_batches[i+1]].toarray(),axis=0)
         neg_tf.append(batch_result)
-        print(neg_batches[i+1],"entries' term frequency calculated")
+        #print(neg_batches[i+1],"entries' term frequency calculated")
         i += 1
 
-    
+
+    #Get positive sentences-> 1
     pos_document_matrix_nostop = cvec.transform(dataset.Sentence[dataset['Class'] == 1])
 
+    #Get positive words into array.
     pos_batches = np.linspace(0,156061,100).astype(int)
     i=0
     pos_tf = []
     while i < len(pos_batches)-1:
         batch_result = np.sum(pos_document_matrix_nostop[pos_batches[i]:pos_batches[i+1]].toarray(),axis=0)
         pos_tf.append(batch_result)
-        print(pos_batches[i+1],"entries' term frequency calculated")
+        #print(pos_batches[i+1],"entries' term frequency calculated")
         i += 1
 
+
+
     #Create dataframe
+    #Columns from neg_tf and pos_tf array
     neg = np.sum(neg_tf,axis=0)
     pos = np.sum(pos_tf,axis=0)
-    term_freq_df2 = pd.DataFrame([neg,pos],columns=cvec.get_feature_names()).transpose()
-    term_freq_df2.columns = ['negative', 'positive']
-    term_freq_df2['total'] = term_freq_df2['negative'] + term_freq_df2['positive']
-    print (term_freq_df2.sort_values(by='total', ascending=False).iloc[:10])
+    #Creates pandas dataframe
+    term_freq_data = pd.DataFrame([neg,pos],columns=cvec.get_feature_names()).transpose()
+    #Write column names
+    term_freq_data.columns = ['negative', 'positive']
+    #Create 'total' column and write its value.
+    term_freq_data['total'] = term_freq_data['negative'] + term_freq_data['positive']
+    #Print term frequencies dataframe by order. (Top 10)
+    print (term_freq_data.sort_values(by='total', ascending=False).iloc[:30])
 
 
 
     #Plotting most used words(negative)
-    y_pos = np.arange(50)
+    y_pos = np.arange(30)
     plt.figure(figsize=(12,10))
-    plt.bar(y_pos, term_freq_df2.sort_values(by='negative', ascending=False)['negative'][:50], align='center', alpha=0.5)
-    plt.xticks(y_pos, term_freq_df2.sort_values(by='negative', ascending=False)['negative'][:50].index,rotation='vertical')
+    plt.bar(y_pos, term_freq_data.sort_values(by='negative', ascending=False)['negative'][:30], align='center', alpha=0.5)
+    plt.xticks(y_pos, term_freq_data.sort_values(by='negative', ascending=False)['negative'][:30].index,rotation='vertical')
     plt.ylabel('Frequency')
-    plt.xlabel('Top 50 negative words')
-    plt.title('Top 50 tokens in negative sentiments')
+    plt.xlabel('Top 30 negative words')
+    plt.title('Top 30 tokens in negative sentiments')
     plt.show()
 
     #Plotting most used words(positive)
-    y_pos = np.arange(50)
+    y_pos = np.arange(30)
     plt.figure(figsize=(12,10))
-    plt.bar(y_pos, term_freq_df2.sort_values(by='positive', ascending=False)['positive'][:50], align='center', alpha=0.5)
-    plt.xticks(y_pos, term_freq_df2.sort_values(by='positive', ascending=False)['positive'][:50].index,rotation='vertical')
+    plt.bar(y_pos, term_freq_data.sort_values(by='positive', ascending=False)['positive'][:30], align='center', alpha=0.5)
+    plt.xticks(y_pos, term_freq_data.sort_values(by='positive', ascending=False)['positive'][:30].index,rotation='vertical')
     plt.ylabel('Frequency')
-    plt.xlabel('Top 50 positive tokens')
-    plt.title('Top 50 tokens in positive sentiments')
+    plt.xlabel('Top 30 positive tokens')
+    plt.title('Top 30 tokens in positive sentiments')
     plt.show()
 
 
     #Scatter plot matrix
     import seaborn as sns
     plt.figure(figsize=(8,6))
-    ax = sns.regplot(x="negative", y="positive",fit_reg=False, scatter_kws={'alpha':0.5},data=term_freq_df2)
+    ax = sns.regplot(x="negative", y="positive",fit_reg=False, scatter_kws={'alpha':0.5},data=term_freq_data)
     plt.ylabel('Positive Frequency')
     plt.xlabel('Negative Frequency')
     plt.title('Negative Frequency vs Positive Frequency')
